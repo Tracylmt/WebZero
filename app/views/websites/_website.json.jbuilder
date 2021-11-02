d5031eeb74bba46c19242232f3ed7b3e7eb2b9f8
Junqi Zhang <junqzhan@redhat.com>
2021-11-02 17:31:17 -0400

    Added something

---
+++ app/views/websites/_website.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! website, :id, :website_address, :username, :created_at, :updated_at
+json.url website_url(website, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :websites
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
